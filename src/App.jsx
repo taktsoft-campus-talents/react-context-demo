551797510cafbd7547a038191a64f598a3334190
Ralf Siewert <ralf@actyvy.st>
2024-02-09 00:47:36 +0100

    cleanup unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styles from "./App.module.css";
 import { Products } from "./components/Products";
 import { Home } from "./components/Home";
